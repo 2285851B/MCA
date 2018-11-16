db0f0d0c5e79ad40db1629c1a882bf274f73079c
2285851B <2285851B@users.noreply.github.com>
2018-11-16 17:25:33 +0000

    javascript update

--- js/index/the_unforgiven.js
+++ js/index/the_unforgiven.js
@@ -1,23 +1,34 @@
 /* This script loads the MEI file via HTTP request */
-
-/* Start Verovio */
-var vrvToolkit = new verovio.toolkit()
-
-/* Setup Verovio - adjust the options below */
-var zoom = 60
+///////////////////////////
+/* Some global variables */
+///////////////////////////
+var vrvToolkit = new verovio.toolkit();
+var zoom = 5;
+var pageHeight = 5;
+var pageWidth = 5;
 var content = document.getElementById('content')
-var contentHeight = 0
+var contentHeight = 10
 if (content) {
   contentHeight = content.offsetHeight
 }
 var defaultHeight = document.documentElement.clientHeight - contentHeight
 var defaultWidth = document.documentElement.clientWidth - 50
-options = {
-    pageHeight: defaultHeight / zoom,
-    pageWidth: defaultWidth / zoom,
-    scale: zoom
+
+///////////////////////////////////////////////////
+/* A function for setting options to the toolkit */
+///////////////////////////////////////////////////
+function setOptions() {
+    //////////////////////////////////////////////////////////////
+    /* Adjust the height and width according to the window size */
+    //////////////////////////////////////////////////////////////
+    pageHeight = $(document).height() * 5 / zoom ;
+    pageWidth = $(window).width() * 5 / zoom ;
+    options = {
+                scale: zoom,
+                adjustPageHeight: true,
+            };
+    vrvToolkit.setOptions(options);
 }
-vrvToolkit.setOptions(options)
 
 /* Show the score */
 showScore('score', 'data/the_unforgiven.mei') // <-- CHANGE THIS TO YOUR FILE
